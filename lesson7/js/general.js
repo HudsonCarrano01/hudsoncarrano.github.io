aee5b2b3d455d322d6d2e4a9a1e6776631248d88
Hudson Carrano <fil19006@byui.edu>
2020-02-21 07:56:44 -0300

    week 7 cont

---
+++ lesson7/js/general.js
@@ -0,0 +1,55 @@
+//window.onload = function() {
+
+// Data sumary Preston City
+var p_currently = 'Rainy';
+var p_high = 54;
+var p_humidity = 95;
+var p_windspeed = 12; 
+var p_windchill = windchill(p_high, p_windspeed);
+
+document.getElementById("currently").innerHTML = p_currently;
+document.getElementById("high").innerHTML      = p_high;
+document.getElementById("humidity").innerHTML  = p_humidity;
+document.getElementById("windspeed").innerHTML = p_windspeed;
+document.getElementById("windchill").innerHTML = p_windchill; 
+
+
+
+// Hide  banner  if is fryday show banner 
+document.getElementById("banner").style.display = "none";
+let dt = new Date();
+let dsem = dt.getDay();
+
+if(dsem == 5){
+    document.getElementById("banner").style.display = "block";
+}
+
+// Change day of week in grid
+let dw = ['Sun','Mon','Tue','Wed','Thu','Fri', 'Sat'];
+let dd = dsem + 1;
+if(dd > 6) dd=0;
+document.getElementById("d1").innerHTML = dw[dd];
+dd++;
+if(dd > 6) dd=0;
+document.getElementById("d2").innerHTML = dw[dd];
+dd++;
+if(dd > 6) dd=0;
+document.getElementById("d3").innerHTML = dw[dd];
+dd++;
+if(dd > 6) dd=0;
+document.getElementById("d4").innerHTML = dw[dd];
+dd++;
+if(dd > 6) dd=0;
+document.getElementById("d5").innerHTML = dw[dd];
+
+
+// Show last modified date
+let update = document.lastModified;
+document.getElementById("lastupdate").innerHTML = update;
+
+//}
+
+// Function to use in nav menu
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
--- lesson7/js/loaderimage.js
+++ lesson7/js/loaderimage.js
@@ -1,8 +1,8 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
@@ -10,23 +10,23 @@ const loadImages = (image) => {
   image.onload = () => {image.removeAttribute('data-src');};
 };
 
-  if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-              loadImages(item.target);
-              observer.unobserve(item.target);
-            }
-          }, imgOptions);
-        });
-        imagesToLoad.forEach((img) => {
-          observer.observe(img);
-        });
-      } else {
-        imagesToLoad.forEach((img) => {
-          loadImages(img);
-        });
-      } 
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      }, imgOptions);
+    });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+} 
  
  
    
