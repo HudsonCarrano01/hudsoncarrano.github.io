00ad519d44e44e519793aa9f872a3c8cf35e7b1b
Hudson Carrano <fil19006@byui.edu>
2020-02-29 17:17:39 -0300

    lesson 8 - cont

--- lesson7/js/general.js
+++ lesson7/js/general.js
@@ -7,41 +7,44 @@ var p_humidity = 95;
 var p_windspeed = 12; 
 var p_windchill = windchill(p_high, p_windspeed);
 
-document.getElementById("currently").innerHTML = p_currently;
-document.getElementById("high").innerHTML      = p_high;
-document.getElementById("humidity").innerHTML  = p_humidity;
-document.getElementById("windspeed").innerHTML = p_windspeed;
-document.getElementById("windchill").innerHTML = p_windchill; 
-
-
-
-// Hide  banner  if is fryday show banner 
-document.getElementById("banner").style.display = "none";
-let dt = new Date();
-let dsem = dt.getDay();
-
-if(dsem == 5){
-    document.getElementById("banner").style.display = "block";
+if(document.getElementById("currently") != "undefined"){
+    document.getElementById("currently").innerHTML = p_currently;
+    document.getElementById("high").innerHTML      = p_high;
+    document.getElementById("humidity").innerHTML  = p_humidity;
+    document.getElementById("windspeed").innerHTML = p_windspeed;
+    document.getElementById("windchill").innerHTML = p_windchill; 
 }
 
-// Change day of week in grid
-let dw = ['Sun','Mon','Tue','Wed','Thu','Fri', 'Sat'];
-let dd = dsem + 1;
-if(dd > 6) dd=0;
-document.getElementById("d1").innerHTML = dw[dd];
-dd++;
-if(dd > 6) dd=0;
-document.getElementById("d2").innerHTML = dw[dd];
-dd++;
-if(dd > 6) dd=0;
-document.getElementById("d3").innerHTML = dw[dd];
-dd++;
-if(dd > 6) dd=0;
-document.getElementById("d4").innerHTML = dw[dd];
-dd++;
-if(dd > 6) dd=0;
-document.getElementById("d5").innerHTML = dw[dd];
+if(document.getElementById("banner")){
 
+    // Hide  banner  if is fryday show banner 
+    document.getElementById("banner").style.display = "none";
+    let dt = new Date();
+    let dsem = dt.getDay();
+
+    if(dsem == 5){
+        document.getElementById("banner").style.display = "block";
+    }
+}
+if(document.getElementById("d1")){
+    // Change day of week in grid
+    let dw = ['Sun','Mon','Tue','Wed','Thu','Fri', 'Sat'];
+    let dd = dsem + 1;
+    if(dd > 6) dd=0;
+    document.getElementById("d1").innerHTML = dw[dd];
+    dd++;
+    if(dd > 6) dd=0;
+    document.getElementById("d2").innerHTML = dw[dd];
+    dd++;
+    if(dd > 6) dd=0;
+    document.getElementById("d3").innerHTML = dw[dd];
+    dd++;
+    if(dd > 6) dd=0;
+    document.getElementById("d4").innerHTML = dw[dd];
+    dd++;
+    if(dd > 6) dd=0;
+    document.getElementById("d5").innerHTML = dw[dd];
+}
 
 // Show last modified date
 let update = document.lastModified;
